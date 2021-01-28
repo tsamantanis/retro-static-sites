7e0656ae1bc560355a5e71e6232d7bc9a139b248
Dani Roxberry <dani@bitoriented.com>
2021-01-28 14:38:27 -0800

    [fix] merge

--- makesite.go
+++ makesite.go
@@ -1,28 +1,7 @@
 package main
 
-<<<<<<< HEAD
-import (
-	"io/ioutil"
-)
-
-func main() {
-
-}
-
-func readFile() string {
-	fileContents, err := ioutil.ReadFile("first-post.txt")
-	if err != nil {
-		panic(err)
-	}
-
-	return string(fileContents)
-}
-
-func renderTemplate()
-=======
 import "fmt"
 
 func main() {
 	fmt.Println("Hello, world!")
 }
->>>>>>> 9514ac8a2c135a448a2b15a4b246dcd5d59ee7bf
