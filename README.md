4a3a23764bc373d9751ef0e83ec3075b9f0df7f7
tsamantanis <philip.tsamantanis@icloud.com>
2021-06-19 05:12:40 -0700

    Generate .html styled files using the --dir flag

--- README.md
+++ README.md
@@ -69,11 +69,11 @@ If you get stuck on any step, be sure to print the output to `stdout`!
 #### v1.1 Requirements
 
 - [x] Create 3 new `.txt` files for testing in the same directory as your project.
-- [ ] Add a new flag to the `makesite` command named `dir`.
-- [ ] Use the flag to find all `.txt` files in the given directory. Print them to `stdout`.
-- [ ] With the list of `.txt` files you found, generate an HTML page for each.
-- [ ] Run `./makesite --dir=.` to test in your local directory.
-- [ ] **Add, commit, and push to GitHub**.
+- [x] Add a new flag to the `makesite` command named `dir`.
+- [x] Use the flag to find all `.txt` files in the given directory. Print them to `stdout`.
+- [x] With the list of `.txt` files you found, generate an HTML page for each.
+- [x] Run `./makesite --dir=.` to test in your local directory.
+- [x] **Add, commit, and push to GitHub**.
 
 #### v1.1 Stretch Challenges
 
--- makesite.go
+++ makesite.go
@@ -2,34 +2,48 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"os"
 	"strings"
 )
 
 type Data struct {
-	Content string
+	Content    string
+	StylesPath string
 }
 
 func main() {
 	filename := flag.String("file", "filename", "name of file to parse")
+	directory := flag.String("dir", "directory", "name of directory of files to parse")
 	flag.Parse()
-	data := loadFileContent(*filename)
-	createHTML(strings.SplitN(*filename, ".", 2)[0], "template.tmpl", data)
+
+	if *directory != "directory" {
+		createManyHTML(*directory)
+	}
+
+	if *filename != "filename" {
+		data := loadFileContent(*filename)
+		createHTML("", strings.SplitN(*filename, ".", 2)[0], "template.tmpl", data)
+	}
+
 }
 
 func loadFileContent(filename string) Data {
-	fileContents, err := ioutil.ReadFile(filename)
+	fileContents, err := os.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
-	return Data{string(fileContents)}
+	var dircount string = ""
+	for i := 0; i < len(strings.SplitN(filename, "/", -1)); i++ {
+		dircount = dircount + "../"
+	}
+	return Data{string(fileContents), dircount + "styles.css"}
 }
 
-func createHTML(filename, templ string, data Data) {
-	htmlFile, osErr := os.Create(filename + ".html")
+func createHTML(dir, filename, templ string, data Data) {
+	htmlFile, osErr := os.Create(dir + "/" + filename + ".html")
 	if osErr != nil {
 		log.Fatal(osErr)
 	}
@@ -38,5 +52,28 @@ func createHTML(filename, templ string, data Data) {
 	execErr := t.Execute(htmlFile, data)
 	if execErr != nil {
 		log.Fatal(execErr)
+	}
+}
+
+func createManyHTML(directory string) {
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range files {
+		path := directory + "/" + file.Name()
+		stat, errStat := os.Stat(path)
+		if errStat != nil {
+			panic(err)
+		}
+		if !stat.IsDir() && strings.SplitN(stat.Name(), ".", 2)[1] == "txt" {
+			data := loadFileContent(path)
+			createHTML(directory, strings.SplitN(file.Name(), ".", 2)[0], "template.tmpl", data)
+		} else if stat.IsDir() {
+			createManyHTML(path)
+		} else {
+			fmt.Println("Error: File type does not match .txt")
+		}
 	}
 }
